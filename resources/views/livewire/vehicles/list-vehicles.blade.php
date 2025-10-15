06cefd0cbcbb6be979445fc1d85db50a4de119e8
Haidar Alessa <10682@holbertonstudents.com>
2025-10-15 17:04:17 +0300

    fix width of create vehicle page to full width.

--- resources/views/livewire/vehicles/list-vehicles.blade.php
+++ resources/views/livewire/vehicles/list-vehicles.blade.php
@@ -1,11 +1,13 @@
 <div class="flex w-full flex-col gap-4">
-    <div class="">
+    <div class="col-span-1 flex items-center justify-between">
         <span class="text-3xl font-medium">My Vehicles</span>
+        <flux:button icon="plus" variant="outline" href="{{ route('vehicles.create') }}">{{ __('Create Vehicle') }}</flux:button>
     </div>
     @if($vehicles->isEmpty())
         <flux:callout icon="car" heading="{{ __('No vehicles yet') }}" subtitle="{{ __('Add your first vehicle to get started.') }}" />
+
     @else
-        <div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
             @foreach($vehicles as $vehicle)
                 <livewire:vehicles.vehicle-card :vehicle="$vehicle" :key="$vehicle->id" />
             @endforeach
