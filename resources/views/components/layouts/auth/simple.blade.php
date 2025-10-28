023e7f33c0a9c6d2f504a8d7c8ce076c3747466f
haidarhalessa <haidarhalessa@gmail.com>
2025-10-28 11:25:50 +0300

    onboarding

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -3,20 +3,25 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-        <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div class="flex w-full max-w-sm flex-col gap-2">
-                <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-                    <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+    <body class="min-h-screen font-montserrat bg-[#f2e9e4] antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+        <div class="flex min-h-svh w-full p-10 gap-10">
+            <div class="relative flex w-2/3">
+                <a href="{{ route('home') }}" class="absolute top-0 left-0" wire:navigate>
+                    <span class="flex items-center gap-2">
+                        <x-app-logo-icon />
+                        <span class="text-3xl text-[#4A4E69] tracking-tighter font-light uppercase">mafhoos</span>
                     </span>
                     <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
                 </a>
-                <div class="flex flex-col gap-6">
+                <div class="flex w-full gap-6 mt-40">
                     {{ $slot }}
                 </div>
             </div>
+            <div class="flex w-1/3 items-center justify-center bg-[#22223b] text-white rounded-3xl">
+                <span>Onboarding</span>
+            </div>
         </div>
+        
         @fluxScripts
     </body>
 </html>
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,11 +1,34 @@
-<div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" />
+<div class="flex flex-col gap-12">
+    <div class="w-96">
+        <flux:heading size="xl">{{ __('How would like to enter your account?') }}</flux:heading>
+    </div>
 
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
 
     <form method="POST" wire:submit="register" class="flex flex-col gap-6">
-        <!-- Name -->
+        <div class="flex items-center gap-4 p-6 bg-white rounded-2xl">
+            @svg('phosphor-whatsapp-logo-light', 'size-14 text-green-500')
+            <div class="flex flex-col">
+                <h4 class="text-xl font-semibold">WhatsApp Number</h4>
+                <span class="text-sm text-[#9A8C98]">Receive a verification code to your WhatsApp number</span>
+            </div>
+        </div>
+        <div class="flex items-center gap-4 p-6 bg-white rounded-2xl">
+            @svg('phosphor-chat-text-light', 'size-14 text-orange-500')
+            <div class="flex flex-col">
+                <h4 class="text-xl font-semibold">SMS Number</h4>
+                <span class="text-sm text-[#9A8C98]">Receive a verification code to your SMS number</span>
+            </div>
+        </div>
+        <div class="flex items-center gap-4 p-6 bg-white rounded-2xl">
+            @svg('phosphor-whatsapp-logo-light', 'size-14 text-green-500')
+            <div class="flex flex-col">
+                <h4 class="text-xl font-semibold">WhatsApp Number</h4>
+                <span class="text-sm text-[#9A8C98]">Receive a verification code to your WhatsApp number</span>
+            </div>
+        </div>
+        {{-- <!-- Name -->
         <flux:input
             wire:model="name"
             :label="__('Name')"
@@ -52,11 +75,11 @@
             <flux:button type="submit" variant="primary" class="w-full">
                 {{ __('Create account') }}
             </flux:button>
-        </div>
+        </div> --}}
     </form>
 
-    <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
+    {{-- <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
         <span>{{ __('Already have an account?') }}</span>
         <flux:link :href="route('login')" wire:navigate>{{ __('Log in') }}</flux:link>
-    </div>
+    </div> --}}
 </div>
