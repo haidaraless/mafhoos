3272b0b863ea32a2bc903a1b688b614fad464957
haidarhalessa <haidarhalessa@gmail.com>
2025-11-06 08:43:18 +0300

    Add icon, color to account type

--- app/Livewire/AccountSwitcher.php
+++ app/Livewire/AccountSwitcher.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire;
 
+use App\Enums\ProviderType;
 use App\Models\Account;
+use App\Models\Provider;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use Livewire\Attributes\Layout;
@@ -25,10 +27,14 @@ class AccountSwitcher extends Component
             ->get()
             ->map(function (Account $account) {
                 $label = $this->makeAccountLabel($account);
+                $icon = $this->getAccountIcon($account);
+                $color = $this->getAccountColor($account);
 
                 return [
                     'id' => $account->id,
                     'label' => $label,
+                    'icon' => $icon,
+                    'color' => $color,
                 ];
             })
             ->all();
@@ -72,6 +78,50 @@ class AccountSwitcher extends Component
         }
 
         return __('Account');
+    }
+
+    private function getAccountIcon(Account $account): string
+    {
+        if ($account->isUser()) {
+            return 'phosphor-user-circle';
+        }
+
+        if ($account->isProvider()) {
+            $provider = $account->accountable;
+            
+            if ($provider instanceof Provider) {
+                return match ($provider->type) {
+                    ProviderType::SPARE_PARTS_SUPPLIER => 'phosphor-package',
+                    ProviderType::AUTO_REPAIR_WORKSHOP => 'phosphor-wrench',
+                    ProviderType::VEHICLE_INSPECTION_CENTER => 'phosphor-clipboard-check',
+                    default => 'phosphor-storefront',
+                };
+            }
+        }
+
+        return 'phosphor-storefront';
+    }
+
+    private function getAccountColor(Account $account): string
+    {
+        if ($account->isUser()) {
+            return 'text-blue-500';
+        }
+
+        if ($account->isProvider()) {
+            $provider = $account->accountable;
+            
+            if ($provider instanceof Provider) {
+                return match ($provider->type) {
+                    ProviderType::SPARE_PARTS_SUPPLIER => 'text-green-500',
+                    ProviderType::AUTO_REPAIR_WORKSHOP => 'text-orange-500',
+                    ProviderType::VEHICLE_INSPECTION_CENTER => 'text-violet-500',
+                    default => 'text-gray-500',
+                };
+            }
+        }
+
+        return 'text-gray-500';
     }
 }
 
--- resources/views/livewire/account-switcher.blade.php
+++ resources/views/livewire/account-switcher.blade.php
@@ -1,15 +1,30 @@
 <div>
     <flux:dropdown position="bottom" align="start">
         <flux:button variant="ghost" class="h-10 px-2">
-            <span class="truncate max-w-48">
-                {{ collect($accounts)->firstWhere('id', $currentAccountId)['label'] ?? __('Account') }}
-            </span>
+            @php
+                $currentAccount = collect($accounts)->firstWhere('id', $currentAccountId);
+            @endphp
+            @if ($currentAccount)
+                <div class="flex items-center gap-2">
+                    @svg($currentAccount['icon'], 'size-5 shrink-0 ' . $currentAccount['color'])
+                    <span class="truncate max-w-48">
+                        {{ $currentAccount['label'] }}
+                    </span>
+                </div>
+            @else
+                <span class="truncate max-w-48">
+                    {{ __('Account') }}
+                </span>
+            @endif
         </flux:button>
 
         <flux:menu>
             @foreach ($accounts as $account)
                 <flux:menu.item wire:click="switch('{{ $account['id'] }}')" :current="$currentAccountId === $account['id']">
-                    <span class="truncate">{{ $account['label'] }}</span>
+                    <div class="flex items-center gap-2">
+                        @svg($account['icon'], 'size-5 shrink-0 ' . $account['color'])
+                        <span class="truncate">{{ $account['label'] }}</span>
+                    </div>
                 </flux:menu.item>
             @endforeach
         </flux:menu>
